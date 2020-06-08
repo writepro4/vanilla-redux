0420607043239e7aae6d595e0066b3b3a03ce8db
writepro4 <writepro4@naver.com>
2020-06-08 10:35:33 +0900

    #1.3 변하는 데이터 출력

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import {createStore} from "redux";
 
 //redux는 데이터를 관리하기 위해 만들어짐.
 //redux에서는 한곳에서만 데이터 수정이 이루어짐.
-
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
+//action을 받아서 조건문으로 실행함
 const countModifier = (count = 0, action) => {
-    console.log(count, action);
     if (action.type === "ADD") {
         return count + 1;
     } else if (action.type === "MINUS") {
@@ -21,13 +22,23 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
+const onChange = () => {
+    number.innerText = countStore.getState();
+};
 
-console.log(countStore.getState());
+
+countStore.subscribe(onChange);
+
+//리덕스에 액션 전달해주는 함수.
+const handleAdd = () => {
+    countStore.dispatch({type: "ADD"});
+};
+const handleMinus = () => {
+    countStore.dispatch({type: "MINUS"});
+};
+
+//이벤트 리스너
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
 
 
