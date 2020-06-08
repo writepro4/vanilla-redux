09a3f8fc990029b8f2ed6cfb0cc0318e9e65220a
writepro4 <writepro4@naver.com>
2020-06-08 10:41:58 +0900

    #1.4 redux 코드 리팩토링

--- src/index.js
+++ src/index.js
@@ -8,14 +8,19 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 //action을 받아서 조건문으로 실행함
+//reducer가 return하는건 어플리케이션의 state가 된다.
 const countModifier = (count = 0, action) => {
-    if (action.type === "ADD") {
-        return count + 1;
-    } else if (action.type === "MINUS") {
-        return count - 1;
-    } else {
-        return count;
+    switch (action.type) {
+        case ADD:
+            return count + 1;
+        case MINUS:
+            return count - 1;
+        default:
+            return count;
     }
 };
 
@@ -30,11 +35,13 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 //리덕스에 액션 전달해주는 함수.
+//dispatch가 reducer에게 데이터를 보내는 역활을 한다.
+//이때 action은 object의 형태로만 내야함. {type:#}
 const handleAdd = () => {
-    countStore.dispatch({type: "ADD"});
+    countStore.dispatch({type: ADD});
 };
 const handleMinus = () => {
-    countStore.dispatch({type: "MINUS"});
+    countStore.dispatch({type: MINUS});
 };
 
 //이벤트 리스너
